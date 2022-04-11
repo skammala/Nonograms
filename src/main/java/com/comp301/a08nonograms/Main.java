7eded0574f5c0ece483ac0bfd8a447c31f910078
skammala <96595074+skammala@users.noreply.github.com>
2022-04-11 17:23:25 -0400

    Add files via upload

---
+++ src/main/java/com/comp301/a08nonograms/Main.java
@@ -0,0 +1,11 @@
+package com.comp301.a08nonograms;
+
+import com.comp301.a08nonograms.view.AppLauncher;
+import javafx.application.Application;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Application.launch(AppLauncher.class);
+  }
+}
